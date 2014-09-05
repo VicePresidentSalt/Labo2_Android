37bf18e036e62ded9c363ee56f44e2b9162d466d
hcfranck <hc_throwdown@hotmail.com>
2014-09-05 14:41:56 -0400

    git

---
+++ gen/com/Prog101/labo2_android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.Prog101.labo2_android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/Prog101/labo2_android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.Prog101.labo2_android;
+
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item);
+    }
+}
